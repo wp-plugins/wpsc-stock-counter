a07c538e0e3022b16023deefc01050d03407b775
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 22:10:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpsc-stock-counter/trunk@96693 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsc-stock-counter.php
+++ wpsc-stock-counter.php
@@ -1,7 +1,8 @@
 <?php
 /*
 Plugin Name: WPSC Stock Counter
-Plugin URI: http://wordpress.org/extend/plugins/wpsc-stock-counter/
+Author URI: http://kolja.galerie-neander.de
+Plugin URI: http://kolja.galerie-neander.de/plugins/#wpsc-stock-counter
 Description: Plugin for <a href="http://www.instinct.co.nz">Wordpress Shopping Cart</a> to count product stock. Products can be combined to be counted together.
 Version: 1.2
 Author: Kolja Schleich
