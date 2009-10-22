9ac4a254f636d746743f873d90e48de997ae66e6
koelle <koelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-22 21:49:33 +0000

    upgrade to wp-e-commerce 3.7.4
    git-svn-id: https://plugins.svn.wordpress.org/wpsc-stock-counter/tags/1.4@166013 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,14 +3,14 @@ Contributors: Kolja Schleich
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2329191
 Tags: shopping cart, tickets, shop, e-commerce, stock counter
 Requires at least: 2.3
-Tested up to: 2.7
-Stable tag: 1.1.2
+Tested up to: 2.8.4
+Stable tag: 1.4
 
 Plugin for the [Shopping Cart Plugin](http://wordpress.org/extend/plugins/wp-e-commerce/) to count stock also of connected products.
 
 == Description ==
 
-This Plugin is for the [Shopping Cart Plugin](http://wordpress.org/extend/plugins/wp-e-commerce/) to count the product stock. Different products can be connected via a comma separated list of their IDs to be counted together.
+This Plugin is for the [Shopping Cart Plugin](http://wordpress.org/extend/plugins/wp-e-commerce/) to count the product stock. Different products can be connected via a comma separated list of their IDs to be counted together. Works with wp-e-commerce 3.7.4.
 
 **Translations**
 
@@ -35,3 +35,7 @@ The WPSC Stock Counter icon is adapted from the Fugue Icons of http://www.pinvok
 
 == Screenshots ==
 1. Administration Page
+
+== Changelog ==
+
+None
--- wpsc-stock-counter.php
+++ wpsc-stock-counter.php
@@ -4,7 +4,7 @@ Plugin Name: WPSC Stock Counter
 Author URI: http://kolja.galerie-neander.de
 Plugin URI: http://kolja.galerie-neander.de/plugins/#wpsc-stock-counter
 Description: Plugin for <a href="http://www.instinct.co.nz">Wordpress Shopping Cart</a> to count product stock. Products can be combined to be counted together.
-Version: 1.2
+Version: 1.4
 Author: Kolja Schleich
 
 Copyright 2007-2008  Kolja Schleich  (email : kolja.schleich@googlemail.com)
@@ -82,7 +82,7 @@ class WPSC_StockCounter
 	{
 		global $wpdb;
 
-		$products = $wpdb->get_results( "SELECT `id`, `name` FROM wp_product_list ORDER BY id ASC" );
+		$products = $wpdb->get_results( "SELECT `id`, `name` FROM {$wpdb->prefix}wpsc_product_list ORDER BY id ASC" );
 		if ( $products ) {
 			foreach ( $products AS $product ) {
 				$this->products[$product->id]['name'] = $product->name;
@@ -106,7 +106,7 @@ class WPSC_StockCounter
 		global $wpdb;
 		
 		$sold = 0;
-		$tickets = $wpdb->get_results( "SELECT `quantity` FROM wp_cart_contents WHERE `prodid` = '".$pid."'" );
+		$tickets = $wpdb->get_results( "SELECT `quantity` FROM {$wpdb->prefix}wpsc_cart_contents WHERE `prodid` = '".$pid."'" );
 		if ( $tickets ) {
 			foreach ( $tickets AS $ticket )
 				$sold += $ticket->quantity;
@@ -114,7 +114,7 @@ class WPSC_StockCounter
 		if ( $this->products[$pid]['linked_products'] != '' ) {
 			$linked_products = explode( ',', $this->products[$pid]['linked_products'] );
 			foreach ( $linked_products AS $l_pid ) {
-				$tickets = $wpdb->get_results( "SELECT `quantity` FROM wp_cart_contents WHERE `prodid` = '".$l_pid."'" );
+				$tickets = $wpdb->get_results( "SELECT `quantity` FROM {$wpdb->prefix}wpsc_cart_contents WHERE `prodid` = '".$l_pid."'" );
 				if ( $tickets ) {
 					foreach ( $tickets AS $ticket ) {
 						$sold += $ticket->quantity;
@@ -172,7 +172,7 @@ class WPSC_StockCounter
 		<div class="wrap">
 			<h2><?php _e( 'Stock Summary', 'wpsc-stock-counter' ) ?></h2>
 			<?php if ( current_user_can('edit_stock_counter_settings') ) : ?>
-				<p style="margin-bottom: 0;"><a href="#" onclick="Element.show('wpsc-stock-counter-settings')"><?php _e( 'Show Settings', 'wpsc-stock-counter' ) ?></a></p>
+				<p style="margin-bottom: 0;"><a href="#" onclick="jQuery('div#wpsc-stock-counter-settings').fadeIn('fast');"><?php _e( 'Show Settings', 'wpsc-stock-counter' ) ?></a></p>
 			<?php endif; ?>
 			
 			<table class="widefat" style="margin-top: 1em;">
@@ -200,7 +200,7 @@ class WPSC_StockCounter
 		<?php if ( current_user_can('edit_stock_counter_settings') ) : ?>
 		<div class="wrap" id="wpsc-stock-counter-settings" style="display: none;">
 			<h2><?php _e( 'Settings', 'wpsc-stock-counter' ) ?></h2>
-			<p><a href="#" onclick="Element.hide('wpsc-stock-counter-settings')"><?php _e( 'Hide Settings', 'wpsc-stock-counter' ) ?></a></p>
+			<p><a href="#" onclick="jQuery('div#wpsc-stock-counter-settings').fadeOut('fast');"><?php _e( 'Hide Settings', 'wpsc-stock-counter' ) ?></a></p>
 			
 			<form action="" method="post">
 				<?php wp_nonce_field( 'wpsc-stock-counter-update-settings_stock' ) ?>
@@ -215,7 +215,7 @@ class WPSC_StockCounter
 						<th scope="col"><?php _e( 'Connected Products', 'wpsc-stock-counter' ) ?>*</th>
 					</tr>
 				</thead>
-				<tbody id="the-list" class="form-table">
+				<tbody class="form-table">
 					<?php foreach ( $this->products AS $pid => $data ) : $selected = ( $this->products[$pid]['count'] == 1 ) ? " checked='checked'" : ''; ?>
 					<?php $class = ( 'alternate' == $class ) ? '' : 'alternate'; ?>
 					<tr class="<?php echo $class ?>">
@@ -268,7 +268,7 @@ class WPSC_StockCounter
 	 */
 	public function addHeaderCode()
 	{
-		wp_print_scripts( 'prototype' );
+		wp_print_scripts( 'jquery' );
 	}
 	
 	
@@ -281,8 +281,10 @@ class WPSC_StockCounter
 	public function addAdminMenu()
 	{
 		$plugin = basename(__FILE__,'.php').'/'.basename(__FILE__);
-		$menu_title = "<img src='".$this->plugin_url."/icon.png' alt='' /> ".__( 'Stock Counter', 'wpsc-stock-counter' );
-	 	$mypage = add_submenu_page( 'wp-shopping-cart/display-log.php', __( 'Stock Counter', 'wpsc-stock-counter' ), $menu_title, 'view_stock_counter', basename(__FILE__), array(&$this, 'printAdminPage') );
+//		$menu_title = "<img src='".$this->plugin_url."/icon.png' alt='' /> ".;
+		$menu_title = __( 'Stock Counter', 'wpsc-stock-counter' );
+
+	 	$mypage = add_submenu_page( 'wpsc-sales-logs', __( 'Stock Counter', 'wpsc-stock-counter' ), $menu_title, 'view_stock_counter', basename(__FILE__), array(&$this, 'printAdminPage') );
 		add_action( "admin_print_scripts-$mypage", array(&$this, 'addHeaderCode') );
 		add_filter( 'plugin_action_links_' . $plugin, array( &$this, 'pluginActions' ) );
 	}
